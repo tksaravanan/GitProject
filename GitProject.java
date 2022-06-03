a1f3fb98a18adc83b548d7f109777335a0f1bbd0
@tksaravanan <tksan25@gmail.com>
2022-06-03 17:40:00 +0530

    echo deleted

--- GitProject.java
+++ GitProject.java
@@ -2,7 +2,6 @@ public class GitProject {
 
  public static void main(String[] args) {
     System.out.println("Hello THAKKU TR KANNAN");
-	 System.out.println("THAKKU TRK");
-	 System.out.println("Happy Birthday PPA");
 	 }
+	 
 }
