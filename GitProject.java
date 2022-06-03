4731959b50a65ed6e3b7fa8afc0dce097594d2ff
@tksaravanan <tksan25@gmail.com>
2022-06-03 17:26:38 +0530

    tks first commit

---
+++ GitProject.java
@@ -0,0 +1,8 @@
+public class GitProject {
+
+ public static void main(String[] args) {
+    System.out.println("Hello THAKKU TR KANNAN");
+	 System.out.println("THAKKU TRK");
+	 System.out.println("Happy Birthday PPA");
+	 }
+}
